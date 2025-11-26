691446b7f10d976a15b64167f6923a907d94d453
Jay A <Jenna.Arends@Seattlecolleges.edu>
2025-11-26 00:57:58 -0800

    Modified Pokemon.jsx, pokemon names are displaying in the UI

--- my-react-app/src/components/Pokemon.jsx
+++ my-react-app/src/components/Pokemon.jsx
@@ -1,12 +1,35 @@
 import { useState } from "react";
 import PokemonDetail from "./PokemonDetail";
 
-const Pokemon = ({}) => {
-    return (
-        <div>
+const Pokemon = ({ currentPokemon }) => {
+    const [selectedPoke, setSelectecPoke] = useState();
 
+    const fetchPokeByID = async (id) => {
+        try {
+            const response = await fetch('https://pokeapi.co/api/v2/pokemon/${id}');
+            if (!response.ok) {
+                throw new Error("Poke lost!"); 
+            }
+            const data = await response.json();
+            setSelectecPoke(data);
+        } catch (error) {
+            console.error("Error fetching Poke :( :", error);
+        }
+    };
+
+    return (
+        <div className="Pokemon">
+            {currentPokemon.map((c, index) => (
+                <div
+                    key = {index}
+                    className = "pokemon"
+                    onClick={() => fetchPokeByID(c.id)} >
+                    {c.name}
+                </div>
+            ))}
+            {selectedPoke && <PokemonDetail pokes= {selectedPoke} />}
         </div>
     );
 };
 
-export default Pokemon
+export default Pokemon;
